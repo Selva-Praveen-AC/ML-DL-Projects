97b00cc12b0e23736c5bed0ab3c37fe3779b9694
selva praveen <selvapraveen5000@gmail.com>
2025-06-25 01:23:40 +0530

    Add database and API in Taxi predict

--- Taxi Predict/main.py
+++ Taxi Predict/main.py
@@ -1,10 +1,14 @@
-from flask import Flask,request,render_template
+from flask import Flask,request,render_template,jsonify
 import numpy as np
 import pickle
-from 
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl','rb'))
+
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/taxi_predict'
+mongo = PyMongo(app)
+db = mongo.db
 
 @app.route('/')
 def home():
@@ -17,7 +21,29 @@ def predict():
         final_value = np.array(input_data).reshape(1,-1)
         prediction = model.predict(final_value)
         output = round(prediction[0],2)
+        
+        db.taxi_predict.insert_one({'Priceperweek':input_data[0],
+                                    'Population':input_data[1],
+                                    'Monthlyincome':input_data[2],
+                                    'Averageparkingpermonth':input_data[3],
+                                    'Predictedfare':output})
         return render_template('index.html', prediction_text='The predicted fare is {}'.format(output))
     
+@app.route('/api',methods=['POST'])
+def api_read():
+    if request.method == 'POST':
+        input_data = request.get_json()
+        final_value = np.array([input_data['Priceperweek'],
+                                input_data['Population'],
+                                input_data['Monthlyincome'],
+                                input_data['Averageparkingpermonth']]).reshape(1,-1)
+        prediction = model.predict(final_value)
+        output = round(prediction[0],2)
+        db.taxi_predict.insert_one({'Priceperweek':input_data['Priceperweek'],
+                                    'Population':input_data['Population'],
+                                    'Monthlyincome':input_data['Monthlyincome'],
+                                    'Averageparkingpermonth':input_data['Averageparkingpermonth'],
+                                    'Predictedfare':output})
+        return jsonify({"Status":"Success","Predicted fair": output })  
 if __name__ == "__main__":
     app.run(debug=True)
